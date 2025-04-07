0581be41d9b1f35ddaba5553d91276bd99d8a1d8
Spencer Post <spencer.post@guildeducation.com>
2025-04-07 09:58:30 -0600

    which key intalled by default

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -48,21 +48,6 @@ return {
   },
 
   {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {},
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show { global = false }
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-    },
-  },
-
-  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
